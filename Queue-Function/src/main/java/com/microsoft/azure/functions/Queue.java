6ec31b73a946e6e6831f3e71382de50862b3fa55
SIRISHA <bharathchandra.abc@gmail.com>
2019-10-21 19:45:42 +0530

    Adding queue function

---
+++ Queue-Function/src/main/java/com/microsoft/azure/functions/Queue.java
@@ -0,0 +1,11 @@
+package com.microsoft.azure.functions;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.*;
+
+public class Queue {
+    @FunctionName("Queue")
+    public void functionHandler(@QueueTrigger(name = "myQueueItem", queueName = "walkthrough", connection = "") String myQueueItem, final ExecutionContext executionContext) {
+        executionContext.getLogger().info("Queue trigger input: " + myQueueItem);
+    }
+}
